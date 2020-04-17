d253da8349cf1e93a6644691410e13e98bc0eddf
Jefferson Rodrigues <rodrigues.jepherson@gmail.com>
2020-04-17 20:15:16 -0300

    Inicio do Projeto

---
+++ pautaApi/src/test/java/br/com/jefferson/pautaApi/PautaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jefferson.pautaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PautaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
